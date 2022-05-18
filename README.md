6184563e7b9ed58ba9e7668bf6cdd4ab202fa164
Riyasat Saber <riyasatsaber@hotmail.com>
2022-05-18 21:37:03 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# anvil 
+
+Using just plain old `yaml` + `jinja2`, anvil aims to be as simple of a static site generator as possible. 
+
+## Quick-start guide 
+
+anvil accepts a project directory file as an input, and an output build directory. The project directory must have the following shape:
+
+```
+my_project_directory/
+├─ templates/               # where Jinja2 Environment will look for templates
+├─ project.yaml             # defining what is to be compiled 
+
+```
+
+```
+anvil my_project_directory build
+```
+
+## Structure of `project.yaml`
+
+```
+>>> project.yaml <<<
+
+# 'pages' is a required key 
+# indicates the relative path of all yaml files to be converted into HTML
+pages: 
+	- my_file_name.yaml
+
+# 'copy' is an optional key
+# a list of files/directories to be copied to the output
+copy:
+  - css
+	- fonts
+```
+
+## `yaml` page
+
+Each input `yaml` page listed in the `pages` key of the `project.yaml` will be rendered into a `html` output. 
+
+```
+>>> my_file_name.yaml <<<
+
+# 'template' is a required key
+# filename of template to use, all templates must be in my_project_directory/templates
+template: base.jinja
+
+# all the keys can be used in the template above during rendering
+key_one: this is the first value
+key_two: this is the second value
+```
+
+## Advanced Examples 
+
+As there is no notion of a file tree or website using anvil, only that one yaml file gets built into one html file, we must use non-standard YAML (like !include) to build functional websites. 
+
+```
+>>> headers.yaml <<<
+links:
+	Home: pages/index.yaml       # Anvil exports a handy function to convert this filename to the generated output path
+	About Me: pages/aboutme.yaml
+	Resume: pages/resume.yaml
+```
+
+```
+>>> aboutme.yaml <<< 
+template: base.jinja
+
+headers: !include headers.yaml   # Every yaml page that would have a header would !include this
+text: "Hey everyone, this is my about me page!"
+```
+
+```
+>>> base.jinja <<< 
+
+{% for title, link in headers.links %}
+	<a href="{{ anvil_link_for(link) }}">{{title}}</a>
+{% endfor %}
+```
+
+
+
---
+++ anvil.py
@@ -0,0 +1,101 @@
+import sys
+import os
+import argparse
+import yaml
+import jinja2
+import shutil
+import errno
+
+DEFAULT_PROJECT_FILE_NAME = "project.yaml"
+
+class Loader(yaml.SafeLoader):
+    def __init__(self, stream):
+        self._root = os.path.split(stream.name)[0]
+        super(Loader, self).__init__(stream)
+
+    def include(self, node):
+        filename = os.path.join(self._root, self.construct_scalar(node))
+        with open(filename, 'r') as f:
+            return yaml.load(f, Loader)
+
+Loader.add_constructor('!include', Loader.include)
+
+class Project():
+    def validate(dictionary):
+        if 'pages' not in dictionary:
+            return "project requires 'pages' key"
+        return None
+
+class Page():
+    def validate(dictionary):
+        if 'template' not in dictionary:
+            return "page requires 'template' key"
+        return None
+
+class Anvil:
+    def __init__(self, project_directory_path, output_directory_path):
+        with open(f'{project_directory_path}/{DEFAULT_PROJECT_FILE_NAME}', 'r') as project:
+            project = yaml.load(project, Loader) 
+            maybe_error = Project.validate(project)
+            if maybe_error is not None:
+                print(f'''{arguments.project_path}: error: {maybe_error}''')
+                sys.exit(1)
+
+        file_loader = jinja2.FileSystemLoader(f'{project_directory_path}/templates')
+        self.environment = jinja2.Environment(loader=file_loader, extensions=['jinja_markdown.MarkdownExtension'])
+
+        self.project_base_path = project_directory_path
+        self.output_directory_path = output_directory_path
+        self.project = project
+
+        def generate_output_page_path(path):
+            filename = path.split('/')[-1]
+            filename_without_extension = filename.split('.')[0]
+            return f'{filename_without_extension}.html'
+
+        self.page_name_mapping = {}
+        for page_path in self.project['pages']:
+            self.page_name_mapping[page_path] = generate_output_page_path(page_path)
+
+    def build(self):
+        for source in (self.project['copy'] if 'copy' in self.project else []):
+            src = f'{self.project_base_path}/{source}'
+            dst = f'{self.output_directory_path}/{source}'
+            try:
+                shutil.copytree(src, dst)
+            except OSError as exception:
+                if exception.errno in (errno.ENOTDIR, errno.EINVAL):
+                    shutil.copy(src, dst)
+                else:
+                    raise
+
+        for page_path in self.project['pages']:
+            self.render_page(page_path)
+
+    def render_page(self, page_path):
+        with open(f'{self.project_base_path}/{page_path}', 'r') as raw_page:
+            loaded_page = yaml.load(raw_page, Loader)
+            maybe_error = Page.validate(loaded_page)
+            if maybe_error is not None:
+                print(f'''{page_path}: error: {maybe_error}''')
+                sys.exit(1)
+
+        template_path = loaded_page['template']
+        template = self.environment.get_template(template_path)
+
+        rendered_html = template.render(**loaded_page)
+        output_path_to_write_to = self.page_name_mapping[page_path]
+        with open(f'{self.output_directory_path}/{output_path_to_write_to}', 'w') as output_file:
+            output_file.write(rendered_html)
+
+def main():
+    parser = argparse.ArgumentParser(description="A really simple static site generator")
+    parser.add_argument("project_path", help="Path to project file")
+    parser.add_argument("-o", "--output", help="Output directory")
+    arguments = parser.parse_args(sys.argv[1:])
+
+    anvil = Anvil(arguments.project_path, arguments.output) 
+    anvil.build()
+
+if __name__ == "__main__":
+    main()
